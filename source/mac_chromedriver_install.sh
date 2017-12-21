0751d878e314464896a8e267c4e9ca4fc0411d67
by-the-w3i <jiangw14@msu.edu>
2017-12-21 04:53:26 -0500

    add mac bash script install instruction

---
+++ source/mac_chromedriver_install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# usage:
+# https://chromedriver.storage.googleapis.com/index.html?path=2.33/
+# sh mac_chromedriver_install.sh
+sudo mkdir /usr/local/
+sudo mkdir /usr/local/bin
+sudo mv ~/Desktop/chromedriver /usr/local/bin
+echo
+echo FINISHED!!!
